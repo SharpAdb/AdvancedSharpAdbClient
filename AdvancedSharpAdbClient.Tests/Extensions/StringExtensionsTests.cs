b7cbae189cf735081785875c122752fd8ebf6996
Seyyid Yiğit <seyyid364@gmail.com>
2023-05-27 13:00:16 +0300

    Added unit tests for string extension methods using Xunit.
    - Added a class StringExtensionsTests that contains two test methods: IsNotNullOrEmpty_ShouldReturnCorrectResult and IsNotNullOrWhiteSpace_ShouldReturnCorrectResult
    - These methods use the [Theory] and [InlineData] attributes to provide multiple test data for the string extension methods
    - These methods assert the expected results using Assert.Equal

---
+++ AdvancedSharpAdbClient.Tests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿using AdvancedSharpAdbClient.Extensions;
+using Xunit;
+
+namespace StringExtensions.Tests
+{
+    public class StringExtensionsTests
+    {
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("", false)]
+        [InlineData(" ", false)]
+        [InlineData("Hello", true)]
+        public void IsNotNullOrEmpty_ShouldReturnCorrectResult(string value, bool expected)
+        {
+            // Arrange
+            // Nothing to arrange
+
+            // Act
+            var result = value.IsNotNullOrEmpty();
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("", false)]
+        [InlineData(" ", false)]
+        [InlineData("Hello", true)]
+        [InlineData(" Hello ", true)]
+        public void IsNotNullOrWhiteSpace_ShouldReturnCorrectResult(string value, bool expected)
+        {
+            // Arrange
+            // Nothing to arrange
+
+            // Act
+            var result = value.IsNotNullOrWhiteSpace();
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
