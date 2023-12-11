ddafe42a62cf1b9f76fd0e87aa953f5680dafb12
wherewhere <qq2518766683@outlook.com>
2023-12-11 18:03:27 +0800

    Use PolySharp to generate attributes

--- AdvancedSharpAdbClient/Polyfills/Attributes/ExcludeFromCodeCoverageAttribute.cs
+++ AdvancedSharpAdbClient/Polyfills/Attributes/ExcludeFromCodeCoverageAttribute.cs
@@ -1,4 +1,4 @@
-﻿#if NET35
+﻿#if NETFRAMEWORK && !NET40_OR_GREATER
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
