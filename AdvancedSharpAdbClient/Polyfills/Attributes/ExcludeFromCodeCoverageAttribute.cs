f10371da2b45785aea4f60cb4ff8d77ec16c4128
where where <qq2518766683@outlook.com>
2026-06-08 18:10:15 +0800

    Upgrade PolySharp to 1.16

---
+++ AdvancedSharpAdbClient/Polyfills/Attributes/ExcludeFromCodeCoverageAttribute.cs
@@ -0,0 +1,19 @@
+﻿#if !NET40_OR_GREATER && !NETCOREAPP && !COMP_NETSTANDARD2_0
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Diagnostics.CodeAnalysis
+{
+    /// <summary>
+    /// Specifies that the attributed code should be excluded from code coverage information.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Constructor | AttributeTargets.Event | AttributeTargets.Interface | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Struct, Inherited = false, AllowMultiple = false)]
+    public sealed class ExcludeFromCodeCoverageAttribute : Attribute
+    {
+        /// <summary>
+        /// Gets or sets the justification for excluding the member from code coverage.
+        /// </summary>
+        public string? Justification { get; set; }
+    }
+}
+#endif
